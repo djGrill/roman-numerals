87428c7d445d64f9bd8651067a0c0d6e23263fc6
David Grilli <djgrill@gmail.com>
2013-12-16 22:44:15 +0000

    Check that the input is valid

--- src/Generator.js
+++ src/Generator.js
@@ -2,33 +2,40 @@ function Generator() {
 }
 
 Generator.prototype.romanNumeralGenerator = function(number) {
-  SYMBOLS = {
-    1: "I",
-    4: "IV",
-    5: "V",
-    9: "IX",
-    10: "X",
-    40: "XL",
-    50: "L",
-    90: "XC",
-    100: "C",
-    400: "CD",
-    500: "D",
-    900: "CM",
-    1000: "M"
-  };
+  number = parseInt(number);
 
-  var result = "";
-  var decimalValues = Object.keys(SYMBOLS).reverse();
+  if (Number.isNaN(number) || number < 1) {
+    return null;
+  } else {
 
-  for (var i = 0; i < decimalValues.length; i += 1) {
-    var decimalValue = parseInt(decimalValues[i]);
+    SYMBOLS = {
+      1: "I",
+      4: "IV",
+      5: "V",
+      9: "IX",
+      10: "X",
+      40: "XL",
+      50: "L",
+      90: "XC",
+      100: "C",
+      400: "CD",
+      500: "D",
+      900: "CM",
+      1000: "M"
+    };
 
-    while (number >= decimalValue) {
-      result += SYMBOLS[decimalValue];
-      number -= decimalValue;
+    var result = "";
+    var decimalValues = Object.keys(SYMBOLS).reverse();
+
+    for (var i = 0; i < decimalValues.length; i += 1) {
+      var decimalValue = parseInt(decimalValues[i]);
+
+      while (number >= decimalValue) {
+        result += SYMBOLS[decimalValue];
+        number -= decimalValue;
+      }
     }
-  }
 
-  return result;
+    return result;
+  }
 };
